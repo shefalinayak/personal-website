7eddb7a1f11b14312d19b27d0b236539829e4536
Shefali Nayak <shefalinayak@gmail.com>
2018-08-23 22:34:13 -0400

    Updates

---
+++ path---blog-b63356faf1af2d63650d.js
@@ -0,0 +1,2 @@
+webpackJsonp([49683490770531],{368:function(e,a){e.exports={data:{allMarkdownRemark:{edges:[{node:{fields:{slug:"/pages/blog/intro/post/"},excerpt:"Before this website update, I had a 2-page personal website written in plain HTML/CSS/JS. It had an animated homepage and a very sad projects page…",id:"/Users/shefalinayak/GitHub/shefalinayak.github.io/src/pages/blog/intro/post.md absPath of file >>> MarkdownRemark",frontmatter:{title:"I finally rebuilt my website!",date:"August 23, 2018"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---blog-b63356faf1af2d63650d.js.map
